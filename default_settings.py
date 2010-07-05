ff553622310a93048fa962a4802c740450c44b28
AloneRoad <AloneRoad@localhost>
2010-07-05 17:44:45 +0700

    ok

--- default_settings.py
+++ default_settings.py
@@ -4,10 +4,4 @@ Created on Jul 5, 2010
 @author: AloneRoad
 '''
 number_of_thread = 2
-users = "users.txt"
 message = "message.txt"
-
-## Skype Sender Account Details
-# 
-username = ""
-password = ""
--- send.py
+++ send.py
@@ -5,15 +5,15 @@
 #  
 # see settings.py and default_settings.py to adjust paramenters
 
-from settings import number_of_thread, users, message
-import workerpool
-import Skype4Py
+from settings import number_of_thread, message
+from workerpool import Job, WorkerPool
+from Skype4Py import Skype
 
 print "Connecting..."
-api = Skype4Py.Skype() # create a Skype API instance 
+api = Skype() # create a Skype API instance 
 api.Attach() # connect to Skype
 
-class Send(workerpool.Job):
+class Send(Job):
   def __init__(self, user, message):
     self.user = user
     self.message = message
@@ -22,12 +22,12 @@ class Send(workerpool.Job):
     api.CreateChatWith(self.user).SendMessage(self.message)  
     
 if __name__ == "__main__":
-  pool = workerpool.WorkerPool(size=number_of_thread)  # create new pool
+  pool = WorkerPool(size=number_of_thread)  # create new pool
   message = open(message).read()
-  users = list(set(open(users).read().split("\n")))
-  
+   
   print "Sending message..."
   print "Total: %s" % api.Friends.Count
+  
   for user in list(api.Friends):
     job = Send(user.Handle, message)
     pool.put(job)
