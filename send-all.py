840ce947c400e39a52b71d9afb9b6a886615ac51
AloneRoad <AloneRoad@localhost>
2010-07-07 10:19:19 +0700

    add counter for monitor status

--- send-all.py
+++ send-all.py
@@ -12,6 +12,8 @@ from Skype4Py import Skype
 print "Connecting..."
 api = Skype() # create a Skype API instance 
 api.Attach() # connect to Skype
+current = None
+total = None
 
 class Send(Job):
   def __init__(self, user, message):
@@ -19,14 +21,19 @@ class Send(Job):
     self.message = message
     
   def run(self):
-    api.CreateChatWith(self.user).SendMessage(self.message)  
+    api.CreateChatWith(self.user).SendMessage(self.message) 
+    print "Sending...  %3.2f" % (current * 100 / float(total))
+    current += 1 
     
 if __name__ == "__main__":
   pool = WorkerPool(size=pool_size)  # create new pool
   message = open(message).read()
    
   print "Sending message..."
-  print "Total: %s" % api.Friends.Count
+  total = api.Friends.Count
+  print "Total: %s" % total
+  
+  current = 0
   
   for user in list(api.Friends):
     job = Send(user.Handle, message)
